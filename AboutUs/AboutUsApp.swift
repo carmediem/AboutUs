f0e46cb13dfb8d216fccb644166955af6629e4b6
carmediem <carchiu@gmail.com>
2022-08-01 10:35:29 -0700

    first commit

---
+++ AboutUs/AboutUsApp.swift
@@ -0,0 +1,20 @@
+//
+//  AboutUsApp.swift
+//  AboutUs
+//
+//  Created by Carmen Chiu on 8/1/22.
+//
+
+import SwiftUI
+
+@main
+struct AboutUsApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
